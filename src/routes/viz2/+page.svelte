065010555d183980113d75d10d3c5f2d20c08283
Colin Dugeai <colin.dugeai@gmail.com>
2025-03-29 16:05:36 +0100

    wip: try using chartjs

---
+++ src/routes/viz2/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { cha } from 'chart.js';
+	import Chart from 'chart.js/auto';
+
+	import {
+		Chart as ChartJS,
+		Title,
+		Tooltip,
+		Legend,
+		LineElement,
+		LinearScale,
+		PointElement,
+		CategoryScale
+	} from 'chart.js';
+	ChartJS.register(Title, Tooltip, Legend, LineElement, LinearScale, PointElement, CategoryScale);
+
+	const config = {
+		type: 'line',
+		data: {},
+		options: {},
+		plugins: []
+	};
+</script>
+
+<Line data={...} />
