b0f2ced01daaf510be2c408ea52fa9d722107ccf
Colin Dugeai <colin.dugeai@gmail.com>
2025-04-03 14:18:30 +0200

    feat: player identity section done

--- src/lib/components/PlayerIdentity.svelte
+++ src/lib/components/PlayerIdentity.svelte
@@ -1,16 +1,50 @@
 <script lang="ts">
-	import { Card } from 'svelte-ux';
+	import { Card, Button } from 'svelte-ux';
+	import profile_12 from '../../../static/profile/12.png';
+
 	interface PlayerIdentityInterface {
 		name: string;
 		age: number;
+		national_team: string;
 		position: string;
+		height: number;
+		debut: string;
+		jersey_no: number;
 	}
 
-	const player: PlayerIdentityInterface = {
-		name: 'John Doe',
-		age: 23,
-		position: 'Attacker (9)'
+	let players: PlayerIdentityInterface[] = [
+		{
+			name: 'Filip Jorgensen',
+			age: 22,
+			national_team: 'Denmark',
+			position: 'Goalkeeper',
+			height: 192,
+			debut: '22 August, 2024',
+			jersey_no: 12
+		},
+		{
+			name: 'Tosin Adarabioyo',
+			age: 27,
+			national_team: 'England',
+			position: 'Centre-back',
+			height: 197,
+			debut: '27 November, 2024',
+			jersey_no: 4
+		}
+	];
+
+	let playerIndex: number = $state(0);
+	const selectNextPlayer = () => {
+		if (playerIndex + 1 === players.length) {
+			playerIndex = 0;
+		} else {
+			playerIndex = playerIndex + 1;
+		}
 	};
+
+	const player: PlayerIdentityInterface = $derived.by(() => {
+		return players[playerIndex];
+	});
 </script>
 
 {#snippet title(text: string)}
@@ -19,14 +53,39 @@
 	</h2>
 {/snippet}
 
-<div class="container">
-	{@render title(player.name)}
+<div class="container items-center">
 	<div class="player-card">
-		<div class="img-container">image</div>
-		<h2>{player.name}</h2>
-		<p>Age: {player.age}</p>
-		<p>Position: {player.position}</p>
+		<div class="img-container">
+			<div class="flex flex-col justify-between">
+				<p class="px-3 py-2 font-extrabold text-6xl font-cfc-color">{player.jersey_no}</p>
+				<div class="flex flex-col mx-1 z-10">
+					<h2 class="text-sm font-light font-cfc-color">{player.name.split(' ')[0]}</h2>
+					<h2 class="text-4xl font-bold font-cfc-color">{player.name.split(' ')[1]}</h2>
+				</div>
+			</div>
+			<img src={'./profile/' + player.jersey_no + '.png'} alt="player picture" />
+		</div>
+
+		<div class="flex gap-2 justify-between my-5" id="player-info">
+			<div class="flex-col font-cfc-color font-bold">
+				<p>NAME</p>
+				<p>AGE</p>
+				<p>POSITION</p>
+				<p>NATIONAL TEAM</p>
+				<p>HEIGHT</p>
+				<p>DEBUT</p>
+			</div>
+			<div class="flex-col font-cfc-color font-light">
+				<p>{player.name}</p>
+				<p>{player.age}</p>
+				<p>{player.position} ({player.jersey_no})</p>
+				<p>{player.national_team}</p>
+				<p>{player.height}</p>
+				<p>{player.debut}</p>
+			</div>
+		</div>
 	</div>
+	<Button variant="outline" on:click={selectNextPlayer}>Next player</Button>
 </div>
 
 <style>
@@ -40,8 +99,28 @@
 	}
 
 	.img-container {
-		height: 200px;
+		display: flex;
+		width: 300px;
+		aspect-ratio: 1;
 		margin: 10px 10px;
-		border: 1px dashed green;
+		border: 1px solid rgba(0, 20, 137, 0.21);
+		border-radius: 5px;
+		overflow: hidden;
+	}
+
+	.img-container img {
+		height: 100%;
+		width: 100%;
+		object-fit: contain;
+		/* or
+              object-fit: cover; */
+		position: relative;
+		top: 10%;
+		left: 0%;
+		z-index: 1;
+	}
+
+	.font-cfc-color {
+		color: #001489;
 	}
 </style>
